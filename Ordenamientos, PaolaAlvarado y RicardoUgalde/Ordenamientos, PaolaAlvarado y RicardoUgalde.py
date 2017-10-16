d504ea0cd1424849d5b73abd286e9c9f3c13a484
Ricardo Ugalde <ugalde.rj@gmail.com>
2017-10-16 17:19:32 -0600

    shell,  radix, heap

--- Ordenamientos, PaolaAlvarado y RicardoUgalde/Ordenamientos, PaolaAlvarado y RicardoUgalde.py
+++ Ordenamientos, PaolaAlvarado y RicardoUgalde/Ordenamientos, PaolaAlvarado y RicardoUgalde.py
@@ -1,16 +1,17 @@
 """
 Ordenamientos, Taller de Programación
 Paola Alvarado y Ricardo Ugalde Gómez
-Burbuja
-Selección
-Inserción
-Shell
-Radix
+#Burbuja
+#Selección
+#Inserción
+#Shell
+#Radix
 Heap-Heapsort
 Shake
 """
 import ast
   # Metodos
+#burbuja
 def ordenamientoBurbuja(lista,tam):
   for i in range(1,tam):
     for j in range(0,tam-i):
@@ -19,7 +20,7 @@ def ordenamientoBurbuja(lista,tam):
         lista[j+1] = lista[j]
         lista[j] = k;
   return lista
-
+#shell
 def ordenShell(lista,tam):
   inc=1
   for inc in range(1,tam,inc*3+1):
@@ -31,9 +32,11 @@ def ordenShell(lista,tam):
           lista[j]=lista[j-inc]
           j=j-inc
         lista[j]=temp
-      inc=inc/2
+      inc=inc//2
   return lista
-
+  print (lista)
+  
+#insercion
 def insercionDirecta(lista,tam):
   for i in range(1,tam):
     v=lista[i]
@@ -43,7 +46,7 @@ def insercionDirecta(lista,tam):
       j=j-1
     lista[j+1]=v
   return lista
-
+#selection
 def selectionsort(lista,tam):
   for i in range(0,tam-1):
     min=i
@@ -54,6 +57,54 @@ def selectionsort(lista,tam):
     lista[min]=lista[i]
     lista[i]=aux
   return lista
+#radix
+def radixsort( aList ):
+  RADIX = 10
+  maxLength = False
+  tmp , placement = -1, 1
+  while not maxLength:
+    maxLength = True
+    # declare and initialize buckets
+    buckets = [list() for _ in range( RADIX )]
+    # split aList between lists
+    for  i in aList:
+      tmp = i // placement
+      buckets[tmp % RADIX].append( i )
+      if maxLength and tmp > 0:
+        maxLength = False
+    # empty lists into aList array
+    a = 0
+    for b in range( RADIX ):
+      buck = buckets[b]
+      for i in buck:
+        aList[a] = i
+        a += 1
+    # move to next digit
+    placement *= RADIX
+  return aList
+
+#Head sort
+def swap(i, j):                    
+    sqc[i], sqc[j] = sqc[j], sqc[i] 
+def heapify(end,i):   
+    l=2 * i + 1  
+    r=2 * (i + 1)   
+    max=i   
+    if l < end and sqc[i] < sqc[l]:   
+        max = l   
+    if r < end and sqc[max] < sqc[r]:   
+        max = r   
+    if max != i:   
+        swap(i, max)   
+        heapify(end, max)   
+def heap_sort():     
+    end = len(sqc)   
+    start = end // 2 - 1 # use // instead of /
+    for i in range(start, -1, -1):   
+        heapify(end, i)   
+    for i in range(end-1, 0, -1):   
+        swap(i, 0)   
+        heapify(i, 0)
 
 #############################SHELL###################################
 
@@ -142,6 +193,45 @@ def mainloop():
           result = ordenShell(lista,tam)
           print("La lista ordenada sería:")
           print(result)
+          print("")
+          
+    if respuesta == "5":
+        print("Usted seleccionó el método: Radix")
+        print("Digite los numeros de la lista:")
+        print("O digite \"s\" para salir.")
+        lista = input()
+        lista = lista.split(",")
+        
+        if "s" in lista or "S" in lista:
+          pass
+
+        else:
+          lista = list(map(int,lista))
+          #tam= len(lista)
+          result = radixsort(lista)
+          print("La lista ordenada sería:")
+          print(result)
+          print("")
+        6
+    if respuesta == "6":
+        print("Usted seleccionó el método: HeapSort")
+        print("Digite los numeros de la lista:")
+        print("O digite \"s\" para salir.")
+        lista = input()
+        lista = lista.split(",")
+        
+        if "s" in lista or "S" in lista:
+          pass
+
+        else:
+          lista = list(map(int,lista))
+          #tam= len(lista)
+          sqc = lista
+          heap_sort()
+          #print(sqc)
+          #result = radixsort(lista)
+          print("La lista ordenada sería:")
+          print(sqc)
           print("")
       
           
